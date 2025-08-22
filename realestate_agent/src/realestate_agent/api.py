eb8b12ca72926fceb923c5f2f9b0e0fc82a78ba7
Muneeb Baig <59503785+MuneebBaig71@users.noreply.github.com>
2025-08-22 23:07:12 +0500

    Updated api.py
    location agent api endpoint added

--- realestate_agent/src/realestate_agent/api.py
+++ realestate_agent/src/realestate_agent/api.py
@@ -4,10 +4,20 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+from realestate_agent.main import is_location_query,location_agent
+
 class UserQuery(BaseModel):
     prompt: str
-
+    
 @app.post("/realestate-agent")
 async def realestate_agent(query: UserQuery):
-    result = await Runner.run(agent, input=query.prompt, session=session)
-    return {"result": result.final_output}
+    if is_location_query(query.prompt):
+        
+        result = await Runner.run(location_agent, input=query.prompt, session=session)
+        print("Location Aggent called")
+        return {"result": result.final_output}
+        
+    else:
+        result = await Runner.run(agent, input=query.prompt, session=session)
+        print("Realestate Agent called")
+        return {"result": result.final_output}
